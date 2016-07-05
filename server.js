92dcd37e5c9ad472ef8bbc4df94ee0900427871b
Joel Busch <jobe.224@gmail.com>
2016-07-05 19:28:54 +0000

    mongoose integrated

--- server.js
+++ server.js
@@ -2,24 +2,19 @@
 
 var express = require('express'),
     routes = require('./app/routes/index.js'),
-    mongo = require('mongodb').MongoClient;
+    mongoose = require('mongoose');
 
 var url = "mongodb://localhost:27017";
 var port = process.env.PORT;
 var app = express();
 
-mongo.connect(url, function (err, db) {
-    if (err) throw new Error('failed to connect');
-    else {
-        console.log('MongoDB connected.');
-    }
+mongoose.connect(url);
     
-    app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-    app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+app.use('/public', express.static(process.cwd() + '/public'));
+
+routes(app);
     
-    routes(app, db);
-    
-    app.listen(port, function () {
-        console.log('Listening on port' + port);
-    });
+app.listen(port, function () {
+    console.log('Listening on port' + port);
 });

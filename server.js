e3f469f52e7ff6534bf14f43d400f801ba503b70
Joel Busch <jobe.224@gmail.com>
2016-07-06 00:34:25 +0000

    github auth system up and working

--- server.js
+++ server.js
@@ -2,18 +2,32 @@
 
 var express = require('express'),
     routes = require('./app/routes/index.js'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    session = require('express-session');
 
 var url = "mongodb://localhost:27017";
-var port = process.env.PORT;
 var app = express();
+require('dotenv').load();
+require('./app/config/passport')(passport);
+var port = process.env.PORT;
 
-mongoose.connect(url);
+mongoose.connect(process.env.MONGO_URI);
     
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/common', express.static(process.cwd() + '/app/common'));
 
-routes(app);
+app.use(session({
+    secret: 'secretClementine',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+routes(app, passport);
     
 app.listen(port, function () {
     console.log('Listening on port' + port);

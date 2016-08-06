edb10a6acf2ac8ecba343d3dcb353df514814f80
Joel Busch <jobe.224@gmail.com>
2016-08-06 19:09:23 +0000

    poll charts now displaying

--- app/controllers/singleController.client.js
+++ app/controllers/singleController.client.js
@@ -2,7 +2,7 @@
 
 (function () {
     
-    var pollInfo = document.querySelector('#single');
+    
     
     var temp = window.location;
     var pid = String(temp).slice(-1);
@@ -11,7 +11,23 @@
     ajaxFunctions.ready(ajaxFunctions.ajaxRequest('GET', apiUrl, function (data) {
         var singlePoll = JSON.parse(data);
         
-        pollInfo.innerHTML = singlePoll.pollTitle.title;
+        google.charts.setOnLoadCallback(drawChart(singlePoll));
     }));
+    
+    function drawChart(singlePoll) {
+        var items = [['Item', 'Votes']];
+        for (var i=0; i<singlePoll.pollItems.length; i++) {
+            items.push([singlePoll.pollItems[i].item, singlePoll.pollItems[i].voteNbr])
+        }
+        
+        var data = google.visualization.arrayToDataTable(items);
+        var options = {
+            title: singlePoll.pollTitle.title
+        };
+        
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+        
+        chart.draw(data, options);
+    }
     
 })();
--- app/controllers/userController.client.js
+++ app/controllers/userController.client.js
@@ -4,15 +4,9 @@
     
     var displayName = document.querySelector('#display-name');
     var apiUrl = appUrl + '/api/:id';
-    
-    function updateHtmlElement (data, element, userProperty) {
-        element.innerHTML = data[userProperty];
-    }
 
     ajaxFunctions.ready(ajaxFunctions.ajaxRequest('GET', apiUrl, function (data) {
         var userObject = JSON.parse(data);
-        
-        updateHtmlElement(userObject, displayName, 'displayName');
         
         if (displayName !== null) {
             displayName.innerHTML = 'Welcome, ' + userObject['displayName'] + "<a href='/profile'><button>Profile</button></a>";  

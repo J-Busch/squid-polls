84ee840416dea31e1cb81132ca964b63b9cb4ae8
Joel Busch <jobe.224@gmail.com>
2016-08-10 05:11:23 +0000

    still fixing heroku stuff

--- app/config/auth.js
+++ app/config/auth.js
@@ -2,8 +2,8 @@
 
 module.exports = {
     'twitterAuth' : {
-        'consumerKey': process.env.TWITTER_KEY,
-        'consumerSecret': process.env.TWITTER_SECRET,
+        'consumerKey': process.env.TWITTER_KEY.toString(),
+        'consumerSecret': process.env.TWITTER_SECRET.toString(),
         'callbackURL': process.env.APP_URL + 'auth/twitter/callback'
     }
 };

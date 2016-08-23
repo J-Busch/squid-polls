fd5054c0670e90b1b276664d9bc743f5248d3b52
Joel Busch <jobe.224@gmail.com>
2016-08-23 05:06:39 +0000

    :^)

--- app/config/passport.js
+++ app/config/passport.js
@@ -16,8 +16,8 @@ module.exports = function (passport) {
     });
     
     passport.use(new TwitterStrategy({
-        consumerKey: configAuth.twitterAuth.consumerKey,
-        consumerSecret: configAuth.twitterAuth.consumerSecret,
+        consumerKey: 'U4uLQAbsqskyWd6TxRbdeE5bF',
+        consumerSecret: 'kCzT0B14Z87SWSDpAI55vF25Wp3r1mCWxaPbjwl1ccoBSygmrg',
         callbackURL: configAuth.twitterAuth.callbackURL
     },
     function (token, refreshToken, profile, done) {
